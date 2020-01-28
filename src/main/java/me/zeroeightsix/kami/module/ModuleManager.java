ab864f3422fbdcfcaa82cd2d8ad2b0a675654e54
xSazo <59794255+xSazo@users.noreply.github.com>
2020-01-28 17:03:58 +0100

    Update ModuleManager.java

--- src/main/java/me/zeroeightsix/kami/module/ModuleManager.java
+++ src/main/java/me/zeroeightsix/kami/module/ModuleManager.java
@@ -1,131 +1,227 @@
 package me.zeroeightsix.kami.module;
 
+import com.google.common.base.Converter;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
 import me.zeroeightsix.kami.KamiMod;
 import me.zeroeightsix.kami.event.events.RenderEvent;
-import me.zeroeightsix.kami.module.modules.ClickGUI;
-import me.zeroeightsix.kami.util.ClassFinder;
-import me.zeroeightsix.kami.util.EntityUtil;
-import me.zeroeightsix.kami.util.KamiTessellator;
-import me.zeroeightsix.kami.util.Wrapper;
+import me.zeroeightsix.kami.gui.kami.KamiGUI;
+import me.zeroeightsix.kami.module.modules.movement.Sprint;
+import me.zeroeightsix.kami.setting.Setting;
+import me.zeroeightsix.kami.setting.Settings;
+import me.zeroeightsix.kami.setting.builder.SettingBuilder;
+import me.zeroeightsix.kami.util.Bind;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.util.math.Vec3d;
-import net.minecraftforge.client.event.RenderWorldLastEvent;
-import org.lwjgl.opengl.GL11;
+import org.lwjgl.input.Keyboard;
 
-import java.lang.reflect.InvocationTargetException;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Set;
+import java.util.List;
 
 /**
  * Created by 086 on 23/08/2017.
+ * Updated by hub on 3 November 2019
  */
-public class ModuleManager {
+public class Module {
 
-    public static ArrayList<Module> modules = new ArrayList<>();
+    private final String originalName = getAnnotation().name();
+    private final Setting<String> name = register(Settings.s("Name", originalName));
+    private final String description = getAnnotation().description();
+    private final Category category = getAnnotation().category();
+    private Setting<Bind> bind = register(Settings.custom("Bind", Bind.none(), new BindConverter()).build());
+    private Setting<Boolean> enabled = register(Settings.booleanBuilder("Enabled").withVisibility(aBoolean -> false).withValue(false).build());
+    public boolean alwaysListening;
+    protected static final Minecraft mc = Minecraft.getMinecraft();
+
+    public List<Setting> settingList = new ArrayList<>();
+
+    public Module() {
+        alwaysListening = getAnnotation().alwaysListening();
+        registerAll(bind, enabled);
+    }
+
+    private Info getAnnotation() {
+        if (getClass().isAnnotationPresent(Info.class)) {
+            return getClass().getAnnotation(Info.class);
+        }
+        throw new IllegalStateException("No Annotation on class " + this.getClass().getCanonicalName() + "!");
+    }
+
+    public void onUpdate() {}
+    public void onRender() {}
+    public void onWorldRender(RenderEvent event) {}
+
+    public Bind getBind() {
+        return bind.getValue();
+    }
+
+    public String getBindName() {
+        return bind.getValue().toString();
+    }
+
+    public void setName(String name) {
+        this.name.setValue(name);
+        ModuleManager.updateLookup();
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public enum Category
+    {
+        COMBAT("Combat", false),
+        EXPLOITS("Exploits", false),
+        RENDER("Render", false),
+        MISC("Misc", false),
+        PLAYER("Player", false),
+        MOVEMENT("Movement", false),
+        HIDDEN("Hidden", true);
+
+        boolean hidden;
+        String name;
+
+        Category(String name, boolean hidden) {
+            this.name = name;
+            this.hidden = hidden;
+        }
+
+        public boolean isHidden() {
+            return hidden;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Info
+    {
+        String name();
+        String description() default "Descriptionless";
+        Module.Category category();
+        boolean alwaysListening() default false;
+    }
+
+    public String getName() {
+        return name.getValue();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public boolean isEnabled() {
+        return enabled.getValue();
+    }
+
+    protected void onEnable() {}
+    protected void onDisable() {}
+
+    public void toggle() {
+        setEnabled(!isEnabled());
+    }
+
+    public void enable() {
+        enabled.setValue(true);
+        onEnable();
+        if (!alwaysListening)
+            KamiMod.EVENT_BUS.subscribe(this);
+    }
+
+    public void disable() {
+        enabled.setValue(false);
+        onDisable();
+        if (!alwaysListening)
+            KamiMod.EVENT_BUS.unsubscribe(this);
+    }
+
+    public boolean isDisabled() {
+        return !isEnabled();
+    }
+
+    public void setEnabled(boolean enabled) {
+        boolean prev = this.enabled.getValue();
+        if (prev != enabled)
+            if (enabled)
+                enable();
+            else
+                disable();
+    }
+
+    public String getHudInfo() {
+        return null;
+    }
+
+    protected final void setAlwaysListening(boolean alwaysListening) {
+        this.alwaysListening = alwaysListening;
+        if (alwaysListening) KamiMod.EVENT_BUS.subscribe(this);
+        if (!alwaysListening && isDisabled()) KamiMod.EVENT_BUS.unsubscribe(this);
+    }
 
     /**
-     * Lookup map for getting by name
+     * Cleanup method in case this module wants to do something when the client closes down
      */
-    static HashMap<String, Module> lookup = new HashMap<>();
+    public void destroy(){};
 
-    public static void updateLookup() {
-        lookup.clear();
-        for (Module m : modules)
-            lookup.put(m.getName().toLowerCase(), m);
+    protected void registerAll(Setting... settings) {
+        for (Setting setting : settings) {
+            register(setting);
+        }
     }
 
-    public static void initialize() {
-        Set<Class> classList = ClassFinder.findClasses(ClickGUI.class.getPackage().getName(), Module.class);
-        classList.forEach(aClass -> {
+    protected <T> Setting<T> register(Setting<T> setting) {
+        if (settingList == null) settingList = new ArrayList<>();
+        settingList.add(setting);
+        return SettingBuilder.register(setting, "modules." + originalName);
+    }
+
+    protected <T> Setting<T> register(SettingBuilder<T> builder) {
+        if (settingList == null) settingList = new ArrayList<>();
+        Setting<T> setting = builder.buildAndRegister("modules." + name);
+        settingList.add(setting);
+        return setting;
+    }
+
+
+    private class BindConverter extends Converter<Bind, JsonElement> {
+        @Override
+        protected JsonElement doForward(Bind bind) {
+            return new JsonPrimitive(bind.toString());
+        }
+
+        @Override
+        protected Bind doBackward(JsonElement jsonElement) {
+            String s = jsonElement.getAsString();
+            if (s.equalsIgnoreCase("None")) return Bind.none();
+            boolean ctrl = false, alt = false, shift = false;
+
+            if (s.startsWith("Ctrl+")) {
+                ctrl = true;
+                s = s.substring(5);
+            }
+            if (s.startsWith("Alt+")) {
+                alt = true;
+                s = s.substring(4);
+            }
+            if (s.startsWith("Shift+")) {
+                shift = true;
+                s = s.substring(6);
+            }
+
+            int key = -1;
             try {
-                Module module = (Module) aClass.getConstructor().newInstance();
-                modules.add(module);
-                // lookup.put(module.getName().toLowerCase(), module);
-            } catch (InvocationTargetException e) {
-                e.getCause().printStackTrace();
-                System.err.println("Couldn't initiate module " + aClass.getSimpleName() + "! Err: " + e.getClass().getSimpleName() + ", message: " + e.getMessage());
-            } catch (Exception e) {
-                e.printStackTrace();
-                System.err.println("Couldn't initiate module " + aClass.getSimpleName() + "! Err: " + e.getClass().getSimpleName() + ", message: " + e.getMessage());
-            }
-        });
-        KamiMod.log.info("Modules initialised");
-        getModules().sort(Comparator.comparing(Module::getName));
-    }
+                key = Keyboard.getKeyIndex(s.toUpperCase());
+            } catch (Exception ignored) {}
 
-    public static void onUpdate() {
-        modules.stream().filter(module -> module.alwaysListening || module.isEnabled()).forEach(module -> module.onUpdate());
-    }
-
-    public static void onRender() {
-        modules.stream().filter(module -> module.alwaysListening || module.isEnabled()).forEach(module -> module.onRender());
-    }
-
-    public static void onWorldRender(RenderWorldLastEvent event) {
-        Minecraft.getMinecraft().profiler.startSection("kami");
-
-        Minecraft.getMinecraft().profiler.startSection("setup");
-//        GlStateManager.pushMatrix();
-        GlStateManager.disableTexture2D();
-        GlStateManager.enableBlend();
-        GlStateManager.disableAlpha();
-        GlStateManager.tryBlendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
-        GlStateManager.shadeModel(GL11.GL_SMOOTH);
-        GlStateManager.disableDepth();
-
-        GlStateManager.glLineWidth(1f);
-        Vec3d renderPos = EntityUtil.getInterpolatedPos(Wrapper.getPlayer(), event.getPartialTicks());
-
-        RenderEvent e = new RenderEvent(KamiTessellator.INSTANCE, renderPos);
-        e.resetTranslation();
-        Minecraft.getMinecraft().profiler.endSection();
-
-        modules.stream().filter(module -> module.alwaysListening || module.isEnabled()).forEach(module -> {
-            Minecraft.getMinecraft().profiler.startSection(module.getName());
-            module.onWorldRender(e);
-            Minecraft.getMinecraft().profiler.endSection();
-        });
-
-        Minecraft.getMinecraft().profiler.startSection("release");
-        GlStateManager.glLineWidth(1f);
-
-        GlStateManager.shadeModel(GL11.GL_FLAT);
-        GlStateManager.disableBlend();
-        GlStateManager.enableAlpha();
-        GlStateManager.enableTexture2D();
-        GlStateManager.enableDepth();
-        GlStateManager.enableCull();
-//        GlStateManager.popMatrix();
-        KamiTessellator.releaseGL();
-        Minecraft.getMinecraft().profiler.endSection();
-
-        Minecraft.getMinecraft().profiler.endSection();
-    }
-
-    public static void onBind(int eventKey) {
-        if (eventKey == 0) return; // if key is the 'none' key (stuff like mod key in i3 might return 0)
-        modules.forEach(module -> {
-            if (module.getBind().isDown(eventKey)) {
-                module.toggle();
-            }
-        });
-    }
-
-    public static ArrayList<Module> getModules() {
-        return modules;
-    }
-
-    public static Module getModuleByName(String name) {
-        return lookup.get(name.toLowerCase());
-//        return getModules().stream().filter(module -> module.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
-    }
-
-    public static boolean isModuleEnabled(String moduleName) {
-        Module m = getModuleByName(moduleName);
-        if (m == null) return false;
-        return m.isEnabled();
+            if (key == 0) return Bind.none();
+            return new Bind(ctrl, alt, shift, key);
+        }
     }
 }
